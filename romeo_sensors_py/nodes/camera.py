a582989089a64aa67276b07077d317631d16e36e
jokla <giovanni.claudio@inria.fr>
2016-04-05 12:03:35 +0200

    remove package romeo_robot

--- romeo_sensors_py/nodes/camera.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import rospy
-from romeo_sensors.romeo_camera import RomeoCam
-
-if __name__ == "__main__":
-  romeocam = RomeoCam('romeo_camera')
-  romeocam.start()
-  rospy.spin()
